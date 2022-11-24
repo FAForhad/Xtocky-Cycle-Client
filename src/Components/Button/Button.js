68148bf1eacc9e4cd2daa34f98088405dd5b2d25
FAForhad <faforhad450@gmail.com>
2022-11-24 10:40:51 +0600

    freatures_responsive

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -3,14 +3,14 @@ import React from 'react';
 const Button = ({ children }) => {
     return (
         <button
-            class="group relative inline-block overflow-hidden border border-yellow-400 px-8 py-3 focus:outline-none focus:ring"
+            class="group relative inline-block overflow-hidden border border-orange-400 px-8 py-3 focus:outline-none focus:ring"
         >
             <span
-                class="absolute inset-y-0 left-0 w-[2px] bg-yellow-400 transition-all group-hover:w-full group-active:bg-yellow-400"
+                class="absolute inset-y-0 left-0 w-[2px] bg-orange-500 transition-all group-hover:w-full group-active:bg-orange-400"
             ></span>
 
             <span
-                class="relative text-sm font-medium text-yellow-400 transition-colors group-hover:text-white"
+                class="relative text-sm font-medium text-yellow-400 transition-colors group-hover:text-black"
             >
                 {children}
             </span>
--- src/Pages/Home/Feature/Feature.js
+++ src/Pages/Home/Feature/Feature.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Button from '../../../Components/Button/Button';
 import './Feature.css'
 
 const Feature = () => {
     return (
         <div className='feature lg:flex my-5 lg:my-12 '>
-            <div className='text-start w-full relative lg:left-32 pt-20 ml-32 mr-56 pl-12 '>
+            <div className='text-start w-full relative lg:left-32 py-6 lg:pt-20 lg:ml-32 mr-24 lg:mr-56 pl-6 lg:pl-12 bg-slate-900'>
                 <h3 className='text-4xl  text-white'>Best to <br /> <span className='text-6xl font-bold text-orange-500' > Check condition's first</span></h3>
-                <p className='py-12'>BEST SELL</p>
+                <p className='my-6 text-white'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy <br /> nibh euismod tincidunt ut laoreet dolore magna aliquam
+                    erat volutpat.<br /> Ut wisi enim ad minim veniam, quis nostrud exerci tation <br /> ullamcorper suscipit lobortis nisl ut aliquip
+                    ex ea </p>
+                <div className='lg:flex  items-center  text-orange-500'  >
+                    <div className='mr-12 my-10'>
+                        <h2 className='py-6 text-3xl'>FEATURED PRODUCTS</h2>
+                        <Link to='/products'>
+                            <Button> Check Products</Button></Link>
+                    </div>
+                    <div>
+                        <p className='py-6 text-3xl'>BEST SELL</p>
+                        <Link to='/products'>
+                            <Button> Check Products</Button></Link>
+                    </div>
+                </div>
             </div>
             <div className='w-full'>
             </div>
